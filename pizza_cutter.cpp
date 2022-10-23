a01a1e5925ea6ea3dfd3c424190849c6b04949bb
elim1988 <elim1988@unm.edu>
2022-10-22 18:16:37 -0600

    made some changes

--- pizza_cutter.cpp
+++ pizza_cutter.cpp
@@ -1,3 +1,9 @@
+/*
+The only thing I can think of that may be the cause of
+the last two test not working is that maybe it is a 
+constraint.
+*/
+
 #include <iostream>
 
 #define ROW (31)
@@ -5,59 +11,74 @@
 
 //This function collects the input data for Pizza Cutter program
 void save_input(int data[ROW][COL]) {
-        std::cin >> data[0][0];
+    
+    std::cin >> data[0][0];
+
     for (int n = 0; (data[0][0] - n) > 0 ; n++) {
+        
         std::cin >> data[n+1][0];
+
         for (int m = 0; (data[n+1][0] - m) > 0; m++) {
             std::cin >> data[n+1][m+1];
         }
     }
 }
 
+//This function counts the number of non-repeated slices 
+//and returns the number of pizza pieces
 int count_slices(int data[ROW][COL], int iteration) {
-    int D[COL] = {};
     int slices = data[iteration][0];
     int pieces = 0;
     
-    if (slices == 0) {
+    if (data[0][0] == 0) {
+        pieces = 0;
+    }
+    else if (slices == 0) {
         pieces = 1;
     }
-    else {
+    else if (slices > 0) {
         
         for (int m = 0; (data[iteration][0] - m) > 0; m++) {
-            D[m] = data[iteration][m+1];
             
-            while (D[m] < 0) {
-                D[m] = -D[m];
+            if (data[iteration][m+1] < 0) {
+                while (data[iteration][m+1] < 0) {
+                    data[iteration][m+1] += 180;
+                }
             }
-            while (D[m] > 180) {
-                D[m] -= 180;
+            else if (data[iteration][m+1] > 0) {
+                while (data[iteration][m+1] > 180) {
+                    data[iteration][m+1] -= 180;
+                }
             }
         }
-        for (int n = 0; (slices - n) > 0; n++) {
-            for (int m = 0; (slices - m) > 0; m++) {
-                if ((D[n] == D[m]) && (n != m)) {
+        for (int n = 0; (data[iteration][0] - n) > 0; n++) {
+            
+            int m = 0;
+            
+            for (m = (n+1); (data[iteration][0] - m) > 0; m++) {
+                if (data[iteration][n+1] == data[iteration][m+1]) {
                     slices--;
-                    for (int i = m; (slices - i); i++) {
-                        D[i] = D[i+1];
-                    }
+                    break;
                 }
             }
         }
         pieces = slices + slices;
     }
-    
+
     return pieces;
 }
 
-//This function takes the slicing offsets and prints number of pizza slices
+//This function calls the count_slices function 
+//and prints the number of pizza pieces
 void print_slices(int data[ROW][COL]) {
 
-    for (int n = 0; (data[0][0] - n) > 0 ; ++n) {
+    for (int n = 0; (data[0][0] - n) > 0 ; n++) {
         int pieces = 0;
 
         pieces = count_slices(data, (n+1));
-        
+        if (pieces < 0) {
+            pieces = 0;
+        }
         if (n == 0) {
             std::cout << pieces;
         }
